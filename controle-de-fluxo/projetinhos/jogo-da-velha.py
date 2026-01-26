2e31f9300fd9a6499baf2b9448b8ccdcf2885967
Adonis Cipriano <desenvolvimento777@gmail.com>
2026-01-26 14:08:02 -0300

    jodo da velha - parte 1

---
+++ controle-de-fluxo/projetinhos/jogo-da-velha.py
@@ -0,0 +1,28 @@
+# exemplo resumido da criação de um tabuleiro:
+# tabuleiro = [ ['' for _ in range(3)] for _ in range(3)]
+
+import os
+os.system('clear')
+
+tabuleiro = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+]
+jogador = 'X'
+
+def exibeTabuleiro():
+  for linha in tabuleiro:
+    print('|'.join(linha))
+    print('-' * 5)
+
+def jogada(linha, coluna):
+  tabuleiro[linha][coluna] = jogador
+  if jogador == 'X':
+    return 'O'
+  else:
+    return 'X'
+  
+jogador = jogada(1,1)
+jogador = jogada(1,2)
+exibeTabuleiro()
