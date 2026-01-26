02962143abe4fde0029cb3ff2e6b62748c09d15f
Adonis Cipriano <desenvolvimento777@gmail.com>
2026-01-26 13:32:37 -0300

    Reutilizando código com funções

---
+++ controle-de-fluxo/funcoes.py
@@ -0,0 +1,14 @@
+def mensagem(mensagem):
+  return(f'{mensagem}')
+
+""" Multiplica dois números """
+def dobro(numero):
+  return numero * 2
+
+""" Soma dois números """
+def somaDoisNumero(a, b):
+  return a + b
+
+print(dobro(5) + 10)
+print(somaDoisNumero(6,3))
+print(mensagem('Olá, mundo! tudo bem?'))
