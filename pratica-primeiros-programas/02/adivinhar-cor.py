acf8df776a22a2981f227ffcd14694d858299f78
Adonis Cipriano <desenvolvimento777@gmail.com>
2026-01-28 13:46:01 -0300

    exercicio 02

---
+++ pratica-primeiros-programas/02/adivinhar-cor.py
@@ -0,0 +1,7 @@
+print("Pense em uma cor, pode ser a sua favorita")
+input("Aperte Enter quando estiver pronto.")
+
+print("Estou lendo sua mente...")
+input()
+
+print("A cor que você pensou é... Azul!")
