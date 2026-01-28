3d54400339c5ada4caf6ba7cee159398d7daf667
Adonis Cipriano <desenvolvimento777@gmail.com>
2026-01-27 23:24:56 -0300

    jogo da velha finalizado

--- controle-de-fluxo/projetinhos/jogo-da-velha.py
+++ controle-de-fluxo/projetinhos/jogo-da-velha.py
@@ -1,6 +1,5 @@
 # exemplo resumido da criação de um tabuleiro:
 # tabuleiro = [ ['' for _ in range(3)] for _ in range(3)]
-
 import os
 os.system('clear')
 
@@ -9,6 +8,7 @@ tabuleiro = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
 ]
+
 jogador = 'X'
 
 def exibeTabuleiro():
@@ -17,12 +17,79 @@ def exibeTabuleiro():
     print('-' * 5)
 
 def jogada(linha, coluna):
-  tabuleiro[linha][coluna] = jogador
-  if jogador == 'X':
-    return 'O'
-  else:
-    return 'X'
+  if (
+    not 0 <= linha <= 2 or 
+    not 0 <= coluna <= 2 or 
+    tabuleiro[linha][coluna] != ' '
+  ):
+    print('Jogada inválida!')
+    return jogador
   
-jogador = jogada(1,1)
-jogador = jogada(1,2)
-exibeTabuleiro()
+  tabuleiro[linha][coluna] = jogador
+  
+  return 'O' if jogador == 'X' else 'X'
+
+def temGanhador():
+  """ veridfica as linhas """
+  for linha in range(3):
+    if(
+      tabuleiro[linha][0] != ' ' and
+      tabuleiro[linha][0] == tabuleiro[linha][1] and
+      tabuleiro[linha][0] == tabuleiro[linha][2]
+    ):
+      print(f'{tabuleiro[linha][0]} GANHOU')
+      return True
+    
+  """ verifica as colunas """
+  for coluna in range(3):
+    if(
+      tabuleiro[0][coluna] != ' ' and
+      tabuleiro[0][coluna] == tabuleiro[1][coluna] and
+      tabuleiro[0][coluna] == tabuleiro[2][coluna]
+    ):
+      print(f'{tabuleiro[0][coluna]} GANHOU')
+      return True
+    
+  """ verifica as diagonais """
+  if (
+      tabuleiro[1][1] != ' ' and
+      (
+        (
+          tabuleiro[0][0] == tabuleiro[1][1] and
+          tabuleiro[0][0] == tabuleiro[2][2]  
+        ) or
+        (
+          tabuleiro[0][2] == tabuleiro[1][1] and
+          tabuleiro[1][1] == tabuleiro[2][0]  
+        )
+        
+      )
+  ):
+    print(f'{tabuleiro[1][1]} GANHOU')
+    return True
+
+  """ se não teve ganhador em nenhuma forma """
+  return False
+
+def temEmpate():
+  for linha in range(3):
+    for coluna in range(3):
+      if tabuleiro[linha][coluna] == ' ':
+        return False
+  print('EMPATE')
+  return True
+
+while True:
+  try:
+    linha = int(input('Digite a linha: '))
+    coluna = int(input('Digite a coluna: '))
+    jogador = jogada(linha, coluna)
+  except (ValueError, IndexError):
+    print('Digite valores númericos entre 0 e 2')
+
+  print(f'Jogador da vez: {jogador}')
+  exibeTabuleiro()
+
+  if temGanhador() or temEmpate():
+      break
+  
