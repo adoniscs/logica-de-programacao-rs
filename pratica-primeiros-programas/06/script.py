991421a477ec0ca2e70d7b1ada909fa1af6c852b
Adonis Cipriano <desenvolvimento777@gmail.com>
2026-02-01 20:29:11 -0300

    inicio do exercicio 06

---
+++ pratica-primeiros-programas/06/script.py
@@ -0,0 +1,11 @@
+import os
+os.system('clear')
+
+perguntas = [['Essa linguagem é fortemente tipara', 'Java']]
+acertos = 0
+
+print('Pense em uma linguagem de programação...')
+
+
+
+print(f'Muito bem!" Você fez {acertos} pontos.')
